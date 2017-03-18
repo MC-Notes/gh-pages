b113d988af5067b5b9ada01ad2cbc4caba7a7042
mzwiessele <ibinbei@gmail.com>
2017-03-18 13:28:32 +0000

    new: added gulp deploy to deploy to gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -83,3 +83,7 @@ gulp.task('watch', function () {
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
+
+gulp.task('deploy', function (done) {
+  return cp.spawn( 'git', ['subtree', 'push', '--prefix', '_site/' ,'origin', 'master'], {stdio: 'inherit'}).on('close', done);
+});
